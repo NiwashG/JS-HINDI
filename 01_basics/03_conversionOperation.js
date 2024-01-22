2182de44441c21d26a34976acc7927fece444a5c
Niwash kumar <125914367+NiwashG@users.noreply.github.com>
2024-01-22 15:43:41 +0000

    mesgae done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+//let score=33//output as number
+//let score="33"//output as string
+//let score="33asd"//output as NaN(not a number)
+//let score=null//output as 0
+//let score=undefined//output as NaN
+//let score=true// output as 1 for boolean value but for false =0
+//let score="NIWASH"//output as NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33"=>33
+//"33asd" NaN
+//true=>1;false=>0
+
+
+//let isLoggedIn=1//output as true
+//let isLoggedIn=""//output as false
+//let isLoggedIn=" "//output as true
+//let isLoggedIn="NIWASH"//output as true
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true; 0 => false
+//"" =>false
+//" "=>true
+//"NIWASH"=>true
+
+
+let someNumber=34
+let stringNumber=String(someNumber)
+console.log(stringNumber)
