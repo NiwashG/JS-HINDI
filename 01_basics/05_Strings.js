e7df656d8c70433b28d15275096485bdda9abe9a
Niwash kumar <125914367+NiwashG@users.noreply.github.com>
2024-01-23 10:37:42 +0000

    string

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,37 @@
+const Name="NIWASH"
+const repoCount=40
+//console.log(Name+ repoCount+" value")
+
+//console.log(`Hello my Name is ${Name} and My repo Count is ${repoCount}`);
+
+//const gameName= new String('Niwash-goldi')
+// console.log(gameName[0])
+// console.log(gameName[2])
+//console.log(gameName.__proto__)//for finding object
+
+// console.log(gameName.length)//for find lengh of charactor
+// console.log(gameName.toUpperCase()) //for uppercase syntax
+// console.log(gameName.charAt('5'));//kis index pe kaun sa index hai
+
+// console.log(gameName.indexOf('w'))//kaun si letter kis index pe hai
+
+//const newString=gameName.substring(0,4)//0 se start index se last se ek kam tak ki letter ko print krega
+
+//console.log(newString);
+
+
+// const anotherString=gameName.slice(-8,2)
+// console.log(anotherString)
+
+// const newStringOne="    Niwash     "
+// console.log(newStringOne)//it will print with space
+// console.log(newStringOne.trim());//if you provide space it will remove
+
+const url="https://www.geeksforgeeks.org/break-and%20-continue-statement-in-java/"
+console.log(url.replace('%20','_'))
+
+console.log(url.includes("geeksforgeeks"))
+
+
+const gameName= new String('Niwash-goldi-com')
+console.log(gameName.split('-'))
